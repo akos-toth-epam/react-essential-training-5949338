b2277da3fbe5aadfbce565e6c71595cdd9841a6c
Akos Toth <akos_toth1@epam.com>
2025-02-17 17:32:43 +0100

    Add contact URL and page

--- next-react-project/src/app/layout.js
+++ next-react-project/src/app/layout.js
@@ -11,9 +11,9 @@ export const metadata = {
 
 function Header() {
   return (
-    <header class="bg-slate-500 py-4">
-      <div class="container mx-auto px-4">
-        <nav class="flex items-center justify-between">
+    <header className="bg-slate-500 py-4">
+      <div className="container mx-auto px-4">
+        <nav className="flex items-center justify-between">
           <div className="flex items-center">
             <Link href="/">Snowtooth Mountain</Link>
           </div>
@@ -22,6 +22,9 @@ function Header() {
           </div>
           <div>
             <Link href="/hotels">Hotels</Link>
+          </div>
+          <div>
+            <Link href="/contact">Contact</Link>
           </div>
         </nav>
       </div>
